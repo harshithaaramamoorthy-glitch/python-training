54700dbd058418698e7577e7467aec6e16aa7056
harshithaaramamoorthy-glitch <harshithaaramamoorthy@gmail.com>
2025-09-25 12:56:46 +0530

    25.9.25
    25.9.25

---
+++ guess.py
@@ -0,0 +1,10 @@
+import random
+guess_number = random.randint(2,5)
+user_num = int(input("your guess number"))
+if(guess_number>=5):
+    print("your guess number is wrong:")
+if(guess_number<=2):
+    print("your guess number is wrong:")
+if(guess_number!=2,5):
+    print("you are lose:")
+    
